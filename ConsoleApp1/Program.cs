abe76935effe9b0eaf5849254f82db4c2844d95a
nikita.ziubin <nikita.ziubin@gmail.com>
2023-02-19 19:34:00 +0200

    add all 8 dunxrions

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,7 @@
 ﻿using ConsoleApp1;
 using System.Text.Json;
 using System.IO;
-
+using System.Collections.Generic;
 
 internal class Program
 {
@@ -31,7 +31,7 @@ internal class Program
         });
         File.WriteAllText(Mpath, json);
     }
-    public static void FindMatch(int id, Matches matches, Stadium[] stadiums )
+    public static void FindMatch(int id, Matches matches, List<Stadium> stadiums )
     {
         foreach (var matche in matches.matchesList)
         {
@@ -67,18 +67,143 @@ internal class Program
     }
     public static void UpdateMatches(int id, Matches matches)
     {
+        Console.Write($"Write item to change: ");
+        string? item = Console.ReadLine();
         foreach (var matche in matches.matchesList)
         {
             if (id == matche.id)
             {
-
+                switch (item)
+                {
+                    case "h":
+                        Console.Write("Enater new homeTeamGoals: ");
+                        int newitem = Convert.ToInt32(Console.ReadLine());
+                        matche.homeTeamGoals = newitem;
+                        break;
+                    case "v":
+                        Console.Write("Enater new visitorTeamGoals: ");
+                        int vitem = Convert.ToInt32(Console.ReadLine());
+                        matche.visitorTeamGoals = vitem;
+                        break;
+                    case "d":
+                        Console.Write("Enater new division: ");
+                        int ditem = Convert.ToInt32(Console.ReadLine());
+                        matche.visitorTeamGoals = ditem;
+                        break;
+                    case "date":
+                        Console.Write("Enater new data: ");
+                        string data = Console.ReadLine();
+                        matche.date = data;
+                        break;
+                    case "s":
+                        Console.Write("Enater new stadiumID: ");
+                        int sitem = Convert.ToInt32(Console.ReadLine());
+                        matche.stadiumID = sitem;
+                        break;
+                    default:
+                        Console.Write("Value didn't match earlier.");
+                        break;
+                }
+                break;
             }
         }
+        var json = JsonSerializer.Serialize(matches, new JsonSerializerOptions
+        {
+            WriteIndented = true
+        });
+        File.WriteAllText(Mpath, json);
+    }
+    public static void AddStadium(List<Stadium> stadiums)
+    {
+        Console.Write("Write adress: ");
+        string adress = Console.ReadLine();
+        Console.Write("Write name: ");
+        string name = Console.ReadLine();
+        Console.Write("Write capacity: ");
+        int capacity = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Write max_capacity: ");
+        int max_capacity = Convert.ToInt32(Console.ReadLine());
+        Stadium stadium = new Stadium(adress, name, capacity, max_capacity);
+
+        stadiums.Add(stadium);
+
+        var json = JsonSerializer.Serialize(stadiums, new JsonSerializerOptions
+        {
+            WriteIndented = true
+        });
+        File.WriteAllText(Spath, json);
+    }
+    public static void FindStadium(int id, List<Stadium> stadiums)
+    {
+        foreach (var stadium in stadiums)
+        {
+            if (id == stadium.id)
+            {
+                Console.WriteLine($"\nadress: {stadium.adress}\n" +
+                    $"name: {stadium.name}\n" +
+                    $"capacity: {stadium.capacity}\n" +
+                    $"max_capacity: {stadium.max_capacity}\n");
+                    break;
+            }
+        }
+
+    }
+    public static void DelleteStadium(int id, List<Stadium> stadiums)
+    {
+        stadiums.RemoveAt(id);
+        var json = JsonSerializer.Serialize(stadiums, new JsonSerializerOptions
+        {
+            WriteIndented = true
+        });
+        File.WriteAllText(Spath, json);
+    }
+    public static void UpdateStadium(int id, List<Stadium> stadiums)
+    {
+        Console.Write($"Write item to change: ");
+        string? item = Console.ReadLine();
+        foreach (var stadium in stadiums)
+        {
+            if (id == stadium.id)
+            {
+                switch (item)
+                {
+                    case "a":
+                        Console.Write("Enater new adress: ");
+                        string newitem = Console.ReadLine();
+                        stadium.adress = newitem;
+                        break;
+                    case "n":
+                        Console.Write("Enater new name: ");
+                        string vitem = Console.ReadLine();
+                        stadium.name = vitem;
+                        break;
+                    case "c":
+                        Console.Write("Enater new capacity: ");
+                        int ditem = Convert.ToInt32(Console.ReadLine());
+                        stadium.capacity = ditem;
+                        break;
+                    case "m":
+                        Console.Write("Enater new data: ");
+                        int mitem = Convert.ToInt32(Console.ReadLine());
+                        stadium.max_capacity = mitem;
+                        break;
+                    default:
+                        Console.Write("Value didn't match earlier.");
+                        break;
+                }
+                break;
+            }
+        }
+        var json = JsonSerializer.Serialize(stadiums, new JsonSerializerOptions
+        {
+            WriteIndented = true
+        });
+        File.WriteAllText(Spath, json);
     }
     private static void Main(string[] args)
     {
         var jsonConect = File.ReadAllText(Spath);
-        var stadiums = JsonSerializer.Deserialize<Stadium[]>(jsonConect, new JsonSerializerOptions
+        var stadiums = JsonSerializer.Deserialize<List<Stadium>>(jsonConect, new JsonSerializerOptions
         {
             PropertyNameCaseInsensitive = true,
         });
@@ -86,8 +211,8 @@ internal class Program
         //Console.WriteLine(stadiums[0]);
         while (true)
         {
-            Console.WriteLine($"(0) Exit \n(1) Add match\n(2) Add Match\n(3) Dellete Match" +
-                $"\n (4) Update Matches");
+            Console.WriteLine($"(0) Exit \n(1) Add Match\n(2) Get match\n(3) Dellete Match" +
+                $"\n(4) Update Matches\n(5) Add Stadium\n(6) Get Stadium\n (7) Dellete Stadium\n(8) Update Stadium");
             int cumNum = Convert.ToInt32(Console.ReadLine());
             if (cumNum == 0)
             {
@@ -111,9 +236,32 @@ internal class Program
             }
             else if (cumNum == 4)
             {
-
+                Console.Write("Warite id: ");
+                int id = Convert.ToInt32(Console.ReadLine());
+                UpdateMatches(id, matches);
             }
-
+            else if (cumNum == 5)
+            {
+                AddStadium(stadiums);
+            }
+            else if (cumNum == 6)
+            {
+                Console.Write("Warite id: ");
+                int id = Convert.ToInt32(Console.ReadLine());
+                FindStadium(id, stadiums);
+            }
+            else if (cumNum == 7)
+            {
+                Console.Write("Warite id: ");
+                int id = Convert.ToInt32(Console.ReadLine());
+                DelleteStadium(id - 1, stadiums);
+            }
+            else if (cumNum == 8)
+            {
+                Console.Write("Warite id: ");
+                int id = Convert.ToInt32(Console.ReadLine());
+                UpdateStadium(id, stadiums);
+            }
         }
     }
 }
--- ConsoleApp1/Stadium.cs
+++ ConsoleApp1/Stadium.cs
@@ -9,16 +9,23 @@ namespace ConsoleApp1
 {
     internal class Stadium
     {
-   
+
         public int id { get; set; }
+        public static int iID;
+
         public string adress { get; set; }
         public string name { get; set; }
         public int capacity { get; set; }
         public int max_capacity { get; set; }
 
-        public Stadium()
+        public Stadium(string adress, string name, int capacity, int max_capacity)
         {
-
+            id = iID + 1;
+            iID++;
+            this.name = name;
+            this.adress = adress;
+            this.capacity = capacity;
+            this.max_capacity = max_capacity;
         }
     }
 }
